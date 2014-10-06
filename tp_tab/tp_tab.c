9feee921d5e2e600be2a24a6a3639aa276fa5a0e
Ministicraft <worldtocraft@gmail.com>
2014-10-06 16:01:38 +0200

    ajout affichage adresse mémoire plus introduction bug boucle infini

--- tp_tab/tp_tab.c
+++ tp_tab/tp_tab.c
@@ -34,6 +34,16 @@ for (i=0 ; i<NB_VAL-1 ; i++)
 	printf ("%d,", tab_val[i]);
 }
 printf("\b \n\n"); // "\b " Remplacement du dernier caractère
+//============================================================================
+// Bug boucle infini
+//============================================================================
+
+/*printf ("Les valeurs saisies dans l'ordre sont:\n");
+for (i=0 ; i<NB_VAL-1 ; i++)
+{
+	printf ("%d,", tab_val[i]);
+	tab_val[-1]=0;
+}*/
 
 //============================================================================
 // Affichage des valeurs dans l'ordre inverse de la saisie
@@ -44,5 +54,29 @@ for (i=NB_VAL-1 ; i>=0 ; i--)
 	printf ("%d,", tab_val[i]);
 }
 printf ("\b \n"); // "\b " Remplacement du dernier caractère
+
+//============================================================================
+// Affichage des adresses mémoires
+//============================================================================
+
+printf ("L'addresse mémoire de val est: %p:\n", &val);
+printf ("L'addresse mémoire de i est: %p:\n", &i);
+printf ("L'addresse mémoire de la somme est: %p:\n", &somme);
+for (i=0 ; i<NB_VAL ; i++)
+{
+	printf ("L'addresse mémoire de la valeur %d du tableau est :%p\n", i+1, &tab_val[i]);
+}
+printf ("Taille d'une case du tableau: %ld\n", sizeof(tab_val[0]));
+printf ("Taille d'un entier: %ld\n", sizeof(int));
+//============================================================================
+// Introduction bug : buffer over flow
+//============================================================================
+printf ("tab_val[10]:%d\n", tab_val[NB_VAL]);
+printf ("L'adresse mémoire de tab_val[10] est:%p\n", &tab_val[NB_VAL]);
+//============================================================================
+// Introduction bug : buffer under flow
+//============================================================================
+printf ("tab_val[-1]:%d\n", tab_val[-1]);
+printf ("L'adresse mémoire de tab_val[-1] est:%p\n", &tab_val[-1]);
 return 0;
 }
